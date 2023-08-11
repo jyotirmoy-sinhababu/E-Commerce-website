a533ac1466c7b7428508aedbabedcccea6a32e9a
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-08-11 16:48:18 +0530

    style added to cart

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,7 +1,7 @@
 import SearchBar from '../../sub-components/searchBar/SearchBar';
 import LogInBtn from '../../sub-components/btns/LogInBtn';
+import CartBtn from '../../sub-components/btns/CartBtn';
 
-import { AiOutlineShoppingCart } from 'react-icons/ai';
 import './navStyle.css';
 
 const Nav = () => {
@@ -11,10 +11,8 @@ const Nav = () => {
         <p className='nav-logo-txt'>E-CART</p>
         <SearchBar />
       </div>
-      <div>
-        <button>
-          <AiOutlineShoppingCart /> Cart
-        </button>
+      <div className='nav-cartBtn-cnt'>
+        <CartBtn />
       </div>
       <div className='nav-logIn-btn-cnt'>
         <LogInBtn />
---
+++ src/sub-components/btns/CartBtn.tsx
@@ -0,0 +1,15 @@
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+
+import './btnStyle.css';
+
+const CartBtn = () => {
+  return (
+    <div className='cartBtn-cnt'>
+      <button className='cartBtn'>
+        <AiOutlineShoppingCart /> Cart
+      </button>
+    </div>
+  );
+};
+
+export default CartBtn;
