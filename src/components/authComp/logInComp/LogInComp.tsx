660b528edecff34b0d88f6c726a1d0552342cb90
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-08-17 23:08:10 +0530

    log in page created

--- src/components/authComp/logInComp/LogInComp.tsx
+++ src/components/authComp/logInComp/LogInComp.tsx
@@ -1,5 +1,27 @@
+import { NavLink } from 'react-bootstrap';
+
 const LogInComp = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <p>Login</p>
+          <p>All yours order are here</p>
+        </div>
+        <form>
+          <label>Your email</label>
+          <input className='' type='email' />
+          <label>Your password</label>
+          <input className='' type='password' />
+          <button>Login</button>
+        </form>
+      </div>
+      <div>
+        <p>Your first time?</p>
+        <NavLink>Sign up</NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default LogInComp;
