c2864afe1c2eab88399bb58dd6c10757fac733d0
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-08-22 15:14:49 +0530

    carouselDtl page created

--- src/components/carousel/CarouselCard.tsx
+++ src/components/carousel/CarouselCard.tsx
@@ -1,8 +1,4 @@
-import { useState } from 'react';
-
 const CarouselCard = ({ data }: any) => {
-  const [imgCount, setImgCount] = useState(0);
-
   return (
     <div className='carouselCard-cnt'>
       <img className='carouselImg' src={data.thumbnail} alt='thumnail' />
---
+++ src/pages/dtlpage/DtlsPage.tsx
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import { dataContext } from '../../assets/dataProvider/DataProvider';
+
+const DtlsPage = () => {
+  const { carouselData } = useContext(dataContext);
+
+  return <div></div>;
+};
+
+export default DtlsPage;
