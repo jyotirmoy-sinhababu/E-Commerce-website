96892180d99040e76ff635bb53b52aa0990fb7f4
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-08-23 00:35:25 +0530

    changes made

--- src/pages/dtlpage/CarouselDtlPage.tsx
+++ src/pages/dtlpage/CarouselDtlPage.tsx
@@ -1,22 +1,49 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { dataContext } from '../../assets/dataProvider/DataProvider';
 
 const CarouselDtlPage = () => {
   const { carouselDtls } = useContext(dataContext);
-  console.log(carouselDtls[0].images);
+
+  const [filterImg, setFIlterImg] = useState<string>();
+  console.log(filterImg);
+
+  const handleImgClick = (i: number) => {
+    const filteredData = carouselDtls[0]?.images?.filter(
+      (item: string, index: number) => {
+        if (index == i) {
+          return item;
+        }
+      }
+    );
+    setFIlterImg(filteredData);
+  };
 
   return (
     <div>
       <div>
-        {carouselDtls[0]?.images?.map((item: string, index: number) => {
-          return (
-            <div key={index}>
-              <img src={item} alt='' />
-            </div>
-          );
-        })}
+        {carouselDtls
+          ? carouselDtls[0]?.images?.map((item: string, index: number) => {
+              return (
+                <div key={index}>
+                  <img
+                    src={item}
+                    alt=''
+                    onClick={() => {
+                      handleImgClick(index);
+                    }}
+                  />
+                </div>
+              );
+            })
+          : null}
       </div>
-      <div></div>
+      <div>
+        {filterImg ? (
+          <img src={filterImg} alt='img' />
+        ) : (
+          <img src='https://i.dummyjson.com/data/products/1/3.jpg' alt='img' />
+        )}
+      </div>
     </div>
   );
 };
