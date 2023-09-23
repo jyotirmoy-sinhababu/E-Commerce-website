2cbd14c1fd904f2642bdcd45fb5466ed4b7338a2
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-09-23 23:42:25 +0530

    intro comp created

---
+++ src/sub-components/introComp/IntroComp.tsx
@@ -0,0 +1,13 @@
+import './introCompStyle.css';
+
+const IntroComp = () => {
+  return (
+    <div className='intro-cnt'>
+      <h1 className='intro-heading'>All your favourite brands are here.</h1>
+
+      <a href='#bottom'>V</a>
+    </div>
+  );
+};
+
+export default IntroComp;
