93b5d48eb35183be81b5bf7e30ca2439f92d3b5b
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-09-24 18:14:33 +0530

    changes made

--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import Nav from '../../components/nav/Nav';
-import IntroComp from '../../sub-components/introComp/IntroComp';
 
 const LandingPage = () => {
   return (
     <div style={{ backgroundColor: '	rgb(252, 249, 238)', height: '100vh' }}>
       <Nav />
-      <IntroComp />
       <Outlet />
     </div>
   );
