5818427d4d6ef04fc85d2fe3989e2b49484e6fde
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2024-02-01 16:29:26 +0530

    all deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,7 @@
 import './App.css';
 
-import LandingPage from './pages/landingPage/LandingPage';
-import Container from './pages/container/Container';
-
-import { Routes, Route } from 'react-router-dom';
-import CarouselDtlPage from './pages/dtlpage/CarouselDtlPage';
-import LogInComp from './components/authComp/logInComp/LogInComp';
-import SignUpComp from './components/authComp/signUpComp/SignUpComp';
-import AllProductPage from './pages/allProductPage/AllProductPage';
-
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path='/' element={<LandingPage />}>
-          <Route index element={<Container />} />
-          <Route path='carouselDtl' element={<CarouselDtlPage />} />
-          <Route path='login' element={<LogInComp />} />
-          <Route path='signUp' element={<SignUpComp />} />
-          <Route path='allProduct' element={<AllProductPage />} />
-        </Route>
-      </Routes>
-    </>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from './assets/store/Store.tsx';
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    {' '}
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>
-  </Provider>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
